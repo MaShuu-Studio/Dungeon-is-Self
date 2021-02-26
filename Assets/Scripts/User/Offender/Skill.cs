e1b2a795c28d605b20109c239b4f2541d7607bd6
cyj <cyj7732@gmail.com>
2021-02-26 11:36:11 +0900

    offender

---
+++ Assets/Scripts/User/Offender/Skill.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Skill
+{
+    public string name {get; private set;}
+    public int readyTurn;
+    public int damage;
+    public string prior;
+
+    public Skill(string name, int readyTurn, int damage, string prior)
+    {
+        this.name = name;
+        this.readyTurn = readyTurn;
+        this.damage = damage;
+        this.prior = prior;
+    }
+    public Skill(string name, int readyTurn, int damage)
+    {
+        this.name = name;
+        this.readyTurn = readyTurn;
+        this.damage = damage;
+        this.prior = "";
+    }
+}
