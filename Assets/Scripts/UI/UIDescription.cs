1b1e1c568ce09ae814205828d68a3518f49a4ac8
Shuu <ghwls328@naver.com>
2021-03-15 17:53:29 +0900

    Basic Monster Skill Description

--- Assets/Scripts/UI/UIDescription.cs
+++ Assets/Scripts/UI/UIDescription.cs
@@ -6,13 +6,12 @@ using UnityEngine.UI;
 public class UIDescription : MonoBehaviour
 {
     private RectTransform rectTransform;
-    // Start is called before the first frame update
-    void Start()
+    
+    void Awake()
     {
-        gameObject.SetActive(false);    
         rectTransform = GetComponent<RectTransform>();    
+        gameObject.SetActive(false);    
     }
-
 
     public void ShowDecription(bool isShow, Vector3 pos)
     {
