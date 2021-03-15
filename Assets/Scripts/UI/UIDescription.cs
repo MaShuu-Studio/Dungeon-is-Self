3db540b77e0a99edaa0130cb60b5380b20dc11e5
Shuu <ghwls328@naver.com>
2021-03-15 17:29:49 +0900

    UI Icon Show Description

---
+++ Assets/Scripts/UI/UIDescription.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIDescription : MonoBehaviour
+{
+    private RectTransform rectTransform;
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameObject.SetActive(false);    
+        rectTransform = GetComponent<RectTransform>();    
+    }
+
+
+    public void ShowDecription(bool isShow, Vector3 pos)
+    {
+        gameObject.SetActive(isShow);
+        rectTransform.anchoredPosition = pos;
+    }
+}
---
+++ Assets/Scripts/UI/UIIcon.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class UIIcon : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    [SerializeField] private UIDescription description;
+    [SerializeField] private Vector3 pos;
+
+    public void OnPointerEnter(PointerEventData pointerEventData)
+    {
+        description.ShowDecription(true, pos);
+    }
+    
+    public void OnPointerExit(PointerEventData pointerEventData)
+    {
+        description.ShowDecription(false, pos);
+    }
+}
