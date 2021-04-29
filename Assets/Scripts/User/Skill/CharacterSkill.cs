1c3ea839f7540a9d8d40395d5a51df529f8730f4
cyj <cyj7732@gmail.com>
2021-04-29 22:01:58 +0900

    CharacterSkill Edit

--- Assets/Scripts/User/Skill/CharacterSkill.cs
+++ Assets/Scripts/User/Skill/CharacterSkill.cs
@@ -9,7 +9,7 @@ namespace Data
         public int damage { get; protected set; }
         public List<int> prior { get; protected set; }
 
-        public CharacterSkill(int id, string name, int turn, int damage, List<int> prior, int tier)
+        public CharacterSkill(int id, int tier, string name, int turn, int damage, List<int> prior)
         {
             this.id = id;
             this.name = name;
@@ -18,14 +18,15 @@ namespace Data
             this.prior = prior;
             this.tier = tier;
         }
-        public CharacterSkill(int id, string name, int turn, int damage)
+        public CharacterSkill(int id, int tier, string name, int turn, int damage)
         {
             this.id = id;
+            this.tier = tier;
             this.name = name;
             this.turn = turn;
             this.damage = damage;
             this.prior = null;
-            this.tier = 1;
+            
         }
     }
 }
